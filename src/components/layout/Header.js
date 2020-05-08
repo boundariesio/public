f35dd95b1491a6ab169263841e7c28b177680da9
Josh Bielick <jbielick@gmail.com>
2020-05-08 15:50:24 -0400

    check window inside function

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,67 +3,68 @@ import AnchorLink from 'react-anchor-link-smooth-scroll';
 import LogoText from '../../svg/LogoText';
 import Button from '../Button';
 
-const onRoot = () => {
-  return typeof window !== 'undefined' && window.location.pathname == '/';
-};
-
 const PricingLink = ({ children }) => (
   <AnchorLink className="px-4" offset={40} href="#pricing">
     {children}
   </AnchorLink>
 );
 
-const Header = () => (
-  <header className="sticky top-0 bg-white shadow z-20">
-    <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
-      <div className="flex items-center">
-        <div className="w-56 sm:w-40">
-          <LogoText />
+const Header = () => {
+  const onRoot = () => {
+    return typeof window !== 'undefined' && window.location.pathname == '/';
+  };
+  return (
+    <header className="sticky top-0 bg-white shadow z-20">
+      <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
+        <div className="flex items-center">
+          <div className="w-56 sm:w-40">
+            <LogoText />
+          </div>
+        </div>
+        <div className="flex mt-4 sm:mt-0">
+          {onRoot() ? (
+            <AnchorLink className="px-4" href="#features">
+              Features
+            </AnchorLink>
+          ) : (
+            <a className="px-4" href="/#features">
+              Features
+            </a>
+          )}{' '}
+          {onRoot() ? (
+            <PricingLink>Pricing</PricingLink>
+          ) : (
+            <a className="px-4" href="/#pricing">
+              Pricing
+            </a>
+          )}{' '}
+          <a className="px-4" href="/docs">
+            API Documentation
+          </a>{' '}
+          {onRoot() ? (
+            <AnchorLink className="px-4" href="#support">
+              Support
+            </AnchorLink>
+          ) : (
+            <a className="px-4" href="/#support">
+              Support
+            </a>
+          )}
+        </div>
+        <div className="hidden md:block">
+          {onRoot() ? (
+            <PricingLink>
+              <Button className="text-sm">Get an API key</Button>
+            </PricingLink>
+          ) : (
+            <a className="px-4" href="/#pricing">
+              Pricing
+            </a>
+          )}
         </div>
       </div>
-      <div className="flex mt-4 sm:mt-0">
-        {onRoot() ? (
-          <AnchorLink className="px-4" href="#features">
-            Features
-          </AnchorLink>
-        ) : (
-          <a className="px-4" href="/#features">
-            Features
-          </a>
-        )}{' '}
-        {onRoot() ? (
-          <PricingLink>Pricing</PricingLink>
-        ) : (
-          <a className="px-4" href="/#pricing">
-            Pricing
-          </a>
-        )}{' '}
-        <a className="px-4" href="/docs">
-          API Documentation
-        </a>{' '}
-        {onRoot() ? (
-          <AnchorLink className="px-4" href="#support">
-            Support
-          </AnchorLink>
-        ) : (
-          <a className="px-4" href="/#support">
-            Support
-          </a>
-        )}
-      </div>
-      <div className="hidden md:block">
-        {onRoot() ? (
-          <PricingLink>
-            <Button className="text-sm">Get an API key</Button>
-          </PricingLink>
-        ) : (
-          <a className="px-4" href="/#pricing">
-            Pricing
-          </a>
-        )}
-      </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 export default Header;
