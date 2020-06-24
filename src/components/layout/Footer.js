a5b6613c52e3fbb28005a6e2caec2eb016f51058
Josh Bielick <jbielick@gmail.com>
2020-06-23 21:55:06 -0400

    add terms and conditions

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 const Footer = () => (
   <footer className="container mx-auto py-3 md:py-12 px-3 mt-20 mb-8 text-gray-800">
@@ -6,22 +7,11 @@ const Footer = () => (
       <div className="flex-1 px-3">
         <h2 className="text-lg font-semibold">About Us</h2>
         <p className="mt-5">
-          We're passionate about creating value from data and we want to help you create value, too.
+          We&apos;re passionate about creating value from data and we want to help you create value, too.
         </p>
         <p>
-          <small>© {new Date().getFullYear()}</small>
+          <small>&copy; boundaries.io {new Date().getFullYear()}</small>
         </p>
-      </div>
-      <div className="hidden flex-1 px-3">
-        <h2 className="text-lg font-semibold">Important Links</h2>
-        <ul className="mt-4 leading-loose">
-          <li>
-            <a href="/terms">Terms &amp; Conditions</a>
-          </li>
-          <li>
-            <a href="/privacy">Privacy Policy</a>
-          </li>
-        </ul>
       </div>
       <div className="flex-1 px-3">
         <h2 className="text-lg font-semibold">Social</h2>
@@ -31,6 +21,14 @@ const Footer = () => (
           </li>
           <li>
             <a href="https://github.com/boundariesio">GitHub</a>
+          </li>
+        </ul>
+      </div>
+      <div className="flex-1 px-3">
+        <h2 className="text-lg font-semibold">Legal</h2>
+        <ul className="mt-4 leading-loose">
+          <li>
+            <Link to="/terms">Terms and Conditions</Link>
           </li>
         </ul>
       </div>
