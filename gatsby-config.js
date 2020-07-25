d5f0ea459126148e1aa6573e19ec699da79262d3
Josh Bielick <jbielick@gmail.com>
2020-07-25 16:10:14 -0400

    add ga regular plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,37 @@
 module.exports = {
   plugins: [
+    // {
+    //   resolve: `gatsby-plugin-gdpr-cookies`,
+    //   options: {
+    //     googleAnalytics: {
+    //       trackingId: 'UA-11539854-16',
+    //       cookieName: 'gatsby-gdpr-google-analytics',
+    //       anonymize: true
+    //     }
+    //   }
+    // },
     {
-      resolve: `gatsby-plugin-gdpr-cookies`,
+      resolve: `gatsby-plugin-google-analytics`,
       options: {
-        googleAnalytics: {
-          trackingId: 'UA-11539854-16',
-          cookieName: 'gatsby-gdpr-google-analytics',
-          anonymize: true,
-        },
-      },
+        trackingId: 'UA-11539854-16',
+        head: false,
+        anonymize: true,
+        respectDNT: true,
+        cookieDomain: 'boundaries.io'
+      }
     },
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-theme-codebushi`,
       options: {
-        tailwindConfig: `tailwind.config.js`,
-      },
+        tailwindConfig: `tailwind.config.js`
+      }
     },
     {
       resolve: 'gatsby-plugin-react-leaflet',
       options: {
-        linkStyles: true,
-      },
-    },
-  ],
+        linkStyles: true
+      }
+    }
+  ]
 };
