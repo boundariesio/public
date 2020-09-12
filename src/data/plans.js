e68528fd453ab80c230f5d38bbce231b7241d259
Josh Bielick <jbielick@gmail.com>
2020-09-12 11:08:37 -0400

    explain API tier in plan features

--- src/data/plans.js
+++ src/data/plans.js
@@ -6,6 +6,7 @@ export default {
     description:
       'Get full access to build your Proof of Concept, test an idea, or use the data on a personal project.',
     features: [
+      'Access to the Community Edition API',
       'No credit card required',
       'Get an API key immediately',
       'Up to 100 API calls per day'
@@ -18,7 +19,12 @@ export default {
     price: 1900,
     description:
       "Ready to use this API in production? We've got you. Subscribe to standard and make sure things are always running smoothly.",
-    features: ['Standard support', 'Request Priority (QoS)', '1K requests per day'],
+    features: [
+      'Access to the most up-to-date data and premium API',
+      'Standard support',
+      'Request Priority (QoS)',
+      '1K requests per day'
+    ],
     formId: '620bf43e-a2bf-4c71-86a8-ed53d5b236fa'
   },
   premium: {
@@ -27,7 +33,12 @@ export default {
     price: 9900,
     description:
       'Your uptime is our uptime. Get premium support and top-tier access for businesses and production applications with lots of usage.',
-    features: ['Premium Support', 'Highest Request Priority (QoS)', '100K requests per day'],
+    features: [
+      'Access to the most up-to-date data and premium API',
+      'Premium Support',
+      'Highest Request Priority (QoS)',
+      '100K requests per day'
+    ],
     formId: 'c4e48a0b-4904-4a7f-8825-bf846cd152a0'
   }
 };
