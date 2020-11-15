d9f80fe4ffb29c418b43846c33a12a2d81e45f5f
Josh Bielick <jbielick@gmail.com>
2020-11-15 18:53:54 -0500

    update logo and copyright

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -7,17 +7,20 @@ const Footer = () => (
       <div className="flex-1 px-3">
         <h2 className="text-lg font-semibold">About Us</h2>
         <p className="mt-5">
-          We&apos;re passionate about creating value from data and we want to help you create value, too.
+          Brought to you by bitmode.
+          <br />
+          We&apos;re passionate about creating value from data and we want to help you create value,
+          too.
         </p>
         <p>
-          <small>&copy; boundaries.io {new Date().getFullYear()}</small>
+          <small>&copy; bitmode, LLC {new Date().getFullYear()}</small>
         </p>
       </div>
       <div className="flex-1 px-3">
         <h2 className="text-lg font-semibold">Social</h2>
         <ul className="mt-4 leading-loose">
           <li>
-            <a href="https://twitter.com/jbielick">Twitter</a>
+            <a href="https://twitter.com/bitmode_xyz">Twitter</a>
           </li>
           <li>
             <a href="https://github.com/boundariesio">GitHub</a>
