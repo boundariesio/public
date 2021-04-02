3b11b4aae9360ee9cf099d3aba2f284385393f21
Josh Bielick <jbielick@users.noreply.github.com>
2021-04-02 15:33:17 -0400

    Update explore.js

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from '../components/layout/public';
 import Explorer from '../components/explorer';
+import { Helmet } from 'react-helmet';
 
 export default () => {
   return (
